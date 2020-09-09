5fdb80abd6cffb0149eebdce55663a58f631056d
merrow-ragan <seragan3@gmail.com>
2020-09-09 13:44:20 -0400

    adds json file for all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,7 @@
 class Api::ProductsController < ApplicationController
+  def product_action
+    @products = Product.all
+    render "all_products.json.jb"
+    
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Wringer Mop Bucket", price: 49.20 , image_path: "https://images-na.ssl-images-amazon.com/images/I/81sl4r4nLsL._SL1500_.jpg"  , description: "Mop bucket and wringer combo features a 35-quart capacity for a more efficient way to clean large areas. Higher back reduces strain to allow comfortable wringing. Lower front offers easier placement of mops to prevent back strain. Smooth surface lets you quickly wipe down the exterior to maintain sanitary use. Nonmarking casters roll smoothly across floors for effortless mobility. " )
+
+Product.create(name: "Commercial Orbiter Floor Machine", price: 302.38, image_path: "https://images-na.ssl-images-amazon.com/images/I/71GzTbqnATL._AC_SL1500_.jpg" , description: "The Commercial Orbiter makes it simple to scrub and polish all floor surfaces as well as deep clean institutional grade carpets. Most conventional floor scrubbers can be difficult to use. That’s why the Orbiter has a brush head that rotates with no torque or gouging in a random orbit for easy-to-use fingertip control.")
+
+Product.create(name: "Welder Machine with Electrode Holder" , price: 97.00, image_path: "https://images-na.ssl-images-amazon.com/images/I/41UyQIDNBAL._AC_.jpg" , description: "The machine is equipped with high-efficiency and low-cost cutting technology, can be widely used in automobile manufacturing, plumbing work, metal processing ,strict construction work, site work, high-quality welding and thin material precision welding.")
