12625637385d6cd7d9932ec83a797080857409c6
merrow-ragan <seragan3@gmail.com>
2020-09-20 23:52:33 -0400

    attempts to show all images, updates products controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,11 +2,40 @@ class Api::ProductsController < ApplicationController
   
   def index
     @products = Product.all
+
+    #if params[:search]
+    # @products = Product.title_search(params[:search])
+
+    if params[:search]
+      @products = @products.where("name iLIKE ?", "%#{params[:search]}%")
+    end
+  
+    if params[:discount]
+      @products = @products.where("price < ?", 10)
+    end
+  
+    if params[:sort] == "price"
+      if params[:sort_order] == "desc"
+        @products = @products.order(price: :desc)
+      else
+        @products = @products.order(:price)
+      end
+    else
+      @products = @products.order(:id)
+    end
+    
+    # @products = Product
+    #   .title_search(params[:search])
+    #   .discounted(params[:discount])
+    #   .sorted(params[:sort], params[:sort_order])
+
     render "index.json.jb"
   end
 
+
   def show
     @product = Product.find(params[:id])
+    @image = Image.find_by(product_id: params[:id])
     render "show.json.jb"
     
   end
@@ -16,36 +45,43 @@ class Api::ProductsController < ApplicationController
       id: params[:id],
       name: params[:name],
       price: params[:price],
-      image_path: params[:image_path],
-      description: params[:description]
-      # quantity: params[:quantity]
+      image: params[:image], # unsure of this 
+      description: params[:description],
+      quantity: params[:quantity]
 
     )
     @product.save
-    # if @product.save
+    if @product.save && @image.save
       render "show.json.jb"
-    # else  
-    #   render json: {errors:@product.errors.full_messages}, status: :unprocessable_entity
-    # end
+    else  
+      render json: {errors:@product.errors.full_messages}, status: :unprocessable_entity
+    end
 
   end
 
 
   def update
     @product = Product.find(params[:id])
+    @image = Image.find_by(product_id: params[:id])
 
     @product.name = params[:name] || @product.name
     @product.price = params[:price] || @product.price
-    @product.image_path = params[:image_path] || @product.image_path
+
+    @image.url = params[:url] || @image.url
+
     @product.description = params[:description] || @product.description
-    # @product.quantity = params[:quantity] || @product.quantity
+    @product.quantity = params[:quantity] || @product.quantity
+    @product.supplier_id = params[:supplier_id] || @product.supplier_id
+
+    @image.url = params[:url] || @image.url 
+
     @product.save
 
-    # if @product.save 
+    if @product.save && @image.save
       render "show.json.jb"
-    # else 
-    #   render json: {errors:@product.errors.full_messages}, status: :unprocessable_entity
-    # end
+    else 
+      render json: {errors:@product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,20 @@
 class Product < ApplicationRecord
 
-  # validates :name, uniqueness: true
-  # validates :description, length: {in: 10..500}
+  validates :name, uniqueness: true, presence: true
+  validates :description, length: {in: 10..500}
   # validates :image_path, presence: true
-  # validates :price, numericality: {greater_than: 0} 
-  # validates :quantity, numericality: {greater_than: 0}
+  validates :price, numericality: {greater_than: 0} 
+  validates :quantity, numericality: {greater_than: 0}
+
+  # scope :title_search, -> (search_term) {where"name iLIKE ?", "%#{params[:search_term]}%"}
+
+  # def supplier # needs to find by supplier id, using Supplier, supplier_id already has attr reader/writer
+  #   Supplier.find_by(id: supplier_id)
+
+  # end 
+
+  belongs_to :supplier
+  has_many :images
   
 
   def is_discounted?
